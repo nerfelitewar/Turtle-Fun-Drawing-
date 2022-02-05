94f6fa2fd907091cd6e0ee2bbc523d45d893c05b
nerfelitewar <80751691+nerfelitewar@users.noreply.github.com>
2022-02-05 12:01:51 +0530

    Add files via upload
    THIS IS MY PYTHON CODE PASTE THIS ON UR IDE AND RUN THIS FUN PROJECT <3
    I HOPE U WILL LIKE IT :)

---
+++ turtle drawing.py
@@ -0,0 +1,111 @@
+#idea of project- DRAWING USING TURTLE FOR FUN OR CHECK KEY AND HAND MOVEMENTS WHILE USING WASD AND C (for circle) 
+#and r,g,b,y,o,v,i & p for color :D
+#use e for eraser and over lap it :D 
+
+import random
+import turtle
+
+wn = turtle.Screen()
+wn.title("TURTLE DRAWING")
+wn.bgcolor("black")
+wn.setup(width=600, height=600)
+
+
+head = turtle.Turtle()
+head.shape("circle")
+head.turtlesize(0.5)
+color=random.choice(["Red","Green","orange","blue", "violet", "indigo","yellow", "purple","black"])
+head.color(color)
+head.direction = 0
+
+def red():
+    head.color("red")
+
+def green():
+    head.color("Green")
+    
+def orange():
+    head.color("orange")
+    
+def blue():
+    head.color("blue")
+    
+def violet():
+    head.color("violet")
+    
+def indigo():
+    head.color("indigo")
+
+def purple():
+    head.color("purple")
+    
+def yellow():
+    head.color("yellow")
+
+def black():
+    head.color("black")
+def white():
+    head.color("white")
+
+def goup():
+    if head.direction != "down":
+        head.direction = "up"
+        move()
+ 
+ 
+def godown():
+    if head.direction != "up":
+        head.direction = "down"
+        move()
+ 
+ 
+def goleft():
+     if head.direction != "right":
+        head.direction = "left"
+        move()
+    
+ 
+def goright():
+    if head.direction != "left":
+        head.direction = "right"
+        move()
+ 
+def move():
+    
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y+20)
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y-20)
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x-20)
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x+20)
+           
+        
+def makecircle():
+    head.circle(50)      
+wn.listen()
+wn.onkeypress(goup, "w")
+wn.onkeypress(godown, "s")
+wn.onkeypress(goleft, "a")
+wn.onkeypress(goright, "d")
+wn.onkeypress(makecircle,"c")
+wn.onkey(red,"r")
+wn.onkey(green,"g")
+wn.onkey(violet,"v")
+wn.onkey(indigo,"i")
+wn.onkey(purple,"p")
+wn.onkey(yellow,"y")
+wn.onkey(orange,"o")
+wn.onkey(black,"e")
+wn.onkey(blue,'b')
+wn.onkey(white,"t")
+turtle.mainloop()
+
+
+
+        
